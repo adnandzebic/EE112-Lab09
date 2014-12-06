b56e51134ff2f42cec3e52c40da446cfc6b0ae74
Ado <adnandzebic@gmail.com>
2014-12-05 21:56:31 -0800

    part 4-1 almost done

--- lab/part1d.m
+++ lab/part1d.m
@@ -1,13 +1,15 @@
 ww_d = 0:pi/10000:pi;
-L_min = 0;
-for L = 40:80
-    t = dtmfdesign([697, 770], L, 8000);
-    h_1 = t(:, 1);
-    HH_1 = freqz(h_1, 1, ww_d);
-    stb_index = find(abs(HH_1) >= 0.25);
-    stb_right = ww_d(stb_index(1) + length(stb_index) - 1);
-    if (2 * pi * 770 / 8000 >= stb_right)
-        L_min = L;
+L_min = 0; % Default
+
+% This loop determines the smallest L that can be used
+for L = 40:100
+    hh = dtmfdesign([697], L, 8000); % Returns a single vector (impulse response)
+    HH = freqz(hh, 1, ww_d); % Generates the frequency response
+    stopband = find(abs(HH) >= 0.25); % Determines the indexes in the stopband
+    stopband_right = ww_d(stopband(length(stopband))); % Determines the right index
+
+    if (2 * pi * 770 / 8000 >= stopband_right) % Checks if 770 Hz is in the stopband
+        L_min = L; % Successfully determined L_min -> Leave loop
         break
     end
 end
