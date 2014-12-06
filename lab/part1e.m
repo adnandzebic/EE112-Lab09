3e919c292fb52550b3123d6acc0c330ca6873893
Ado <adnandzebic@gmail.com>
2014-12-06 13:50:29 -0800

    finished

--- lab/part1e.m
+++ lab/part1e.m
@@ -1,7 +1,7 @@
 all_frequencies = [697 770 852 941 1209 1336 1477 1633];
 ww = 0:pi/10000:pi;
 fs = 8000; % Sampling rate is still set at 8000
-L = 40; % Should result in a good passing bandwidth
+L = 80; % Should result in a good passing bandwidth
 A = dtmfdesign(all_frequencies, L, fs); % Generate a matrix of impulse responses
 HH = zeros(length(ww), 8); % Instantiating a matrix of frequency responses
 
@@ -14,8 +14,8 @@ end
 figure(1)
 plot(ww, abs(HH));
 hold on
-line([0,pi], [0.707,0.707],'Color', 'r', 'LineStyle', ':')
-title('Eight Bandpass Filters at Different Frequencies with L=40')
+line([0,pi], [0.707,0.707],'Color', 'g', 'LineStyle', ':')
+title('Eight Bandpass Filters at Different Frequencies with L=80')
 ylabel('Amplitude')
 xlabel('Normalized Frequency')
 hold off
